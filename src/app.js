3bc7e1d1470fe28b0ca7998336ffdfdcd0bb67fb
WangechiG89 <105557035+WangechiG89@users.noreply.github.com>
2022-06-12 21:59:00 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,34 +12,42 @@ let days=["Sunday", "Monday", "Tuesday", "Wednesaday", "Thursday", "Friday", "Sa
 let day=days [date.getDay()];
     return`${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+  return days[day];
+
+}
 function displayForecast(response){
-  console.log(response.data.daily);
+  let forecast=response.data.daily;
     let forecastElement=document.querySelector("#forecast");
 
     let forecastHTML=`<div class="row">`;
     let days =["Thur", "Fri", "Sat","Sun"];
-    days.forEach(function(day){
-
+    forecast.forEach(function(forecastDay, index){
+    if (index<6){
     forecastHTML=forecastHTML+`
            
               <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
-
+                <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+                    ${index}
                 <img
-                  src="https://openweathermap.org/img/wn/50n@2x.png"
+                  src="https://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon}@2x.png"
                   alt=""
                   width="36"
                 />
                 <div class="weather-forecast-temperature">
-                  <span class="weather-forecast-temperature-max">18° </span>
-                  <span class="weather-forecast-temperature-mini">12° </span>
+                  <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}° </span>
+                  <span class="weather-forecast-temperature-mini">${Math.round(forecastDay.temp.min)}° </span>
                 </div>
               </div>
             </div>
              `;
 
-
+    }
 
     });
       
